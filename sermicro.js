a5e1d7761001d36c869a5ba47376f28af15afa47
Alberto Francisco <franiglal@gmail.com>
2018-12-12 01:13:02 +0100

    añadiendo funcionalidad de tipo de aviso

--- sermicro.js
+++ sermicro.js
@@ -10,9 +10,9 @@ var direccion = document.getElementById("direccion");
 var telefono = document.getElementById("telefono")
 var horario = document.getElementById("horario");
 var modelo = document.getElementById("modelo");
-//var smat = document.getElementById("smat");
-//var sat = document.getElementsById("sat");
-//var urgente = document.getElementById("urgente");
+var aviso = "";
+var urgente = document.getElementById("urgente");
+var p1 ="";
 var descripcion = document.getElementById("descripcion");
 var creaccion = document.getElementById("creacion");
 var plantilla = document.getElementById("plantilla");
@@ -20,10 +20,28 @@ var plantilla = document.getElementById("plantilla");
 
 
 creaccion.addEventListener("click", generarPlantilla);
+urgente.addEventListener("onclick", ponerAP1);
+
+function ponerAP1(){
+	console.log(urgente.checked);
+	if (urgente.checked) {
+		p1 = "P1 - ";
+		console.log("checked pulsado");
+	}
+}
 
 function generarPlantilla() {
+
+	if (document.getElementById("smat").checked){
+		aviso = "SMANT";
+		console.log("tecla smant");
+	}else if (document.getElementById("sat").checked) {
+		aviso = "SATS01";
+		console.log("telca sat");
+	}
+	
 	plantilla.innerHTML = "+++++++++++++++++++++++++++++++++++++++++++++++++++ <br />"
-	 + "<br /> asunto: Oceane: " + oceane.value + " - " + empresa.value +  "<br />"
+	 + "<br /> asunto:" + p1 + " Oceane: " + oceane.value + " - " + empresa.value +  "<br />"
 	 + "<br />" + " - nº de ticket de ORANGE: " + oceane.value
 	 + "<br />" + " - datos de cliente: <br />"
 	 + "    nombre: " + nombre.value  + "<br />"
@@ -31,7 +49,7 @@ function generarPlantilla() {
 	 + "    Telefono: " + telefono.value  + "<br />"
 	 + " - horario de atención del cliente: " + horario.value + "<br />"
 	 + " - plantilla: no <br />  - modelo de equipo instalado (incluida tarjetería): " + modelo.value
-	 + "<br />" + " - tipo de aviso: " + "smnat"
+	 + "<br />" + " - tipo de aviso: " + aviso
 	 + "<br />" + " - descripción de la solicitud: <br />"
 	 + descripcion.value
 	 + "<br /";
