3037b480ffd0b6c5ab36039da53cb09db3e35a0f
Alberto Francisco <franiglal@gmail.com>
2018-12-28 00:08:03 +0100

    creada plantilla TESA

--- sermicro.js
+++ sermicro.js
@@ -4,12 +4,16 @@
 
 
 var oceane = document.getElementById("oceane");
-var empresa = document.getElementById("incidencia");
-var nombre = document.getElementById("admin");
-var  = document.getElementById("fecha");
+var incidencia = document.getElementById("incidencia");
+var admin = document.getElementById("admin");
+var fecha = document.getElementById("fecha");
 var horario = document.getElementById("horario");
 var aviso = 1;
-var correo ="";
+var correo = "";
+var nivel = "";
+var causa = document.getElementById("causa");
+var circuito = "";
+var creaccion = document.getElementById("creacion");
 
 creaccion.addEventListener("click", generarPlantilla);
 
@@ -18,52 +22,52 @@ function generarPlantilla() {
 
 	//seleccionar tipo de escalado
 	if (document.getElementById("N1").checked){
-		correo = "escalado.obadirecto@telefonica.com";
+		nivel = "N1";
 		aviso = 1;
 		console.log("tecla smant");
 	}else if (document.getElementById("N2").checked) {
+		nivel = "N2";
 		aviso = 2;
-		correo = "responsable.obadirecto@telefonica.com";
 		console.log("telca sat");
 	}
 
 	//selecionar escalado
 	if (document.getElementById("directo").checked) {
+		circuito = "ULLADSL/SHDSL";
 		if(aviso == 1){
 			correo = "escalado.obadirecto@telefonica.com";
 		}else{
 			correo = "responsable.obadirecto@telefonica.com";
 		}
 	}else if (document.getElementById("indirecto").checked) {
+		circuito = "ADSL-IP/GIGADSL";
 		if(aviso == 1){
-			correo = "escalado.obadirecto@telefonica.com";
+			correo = "escalado.obaindirecto@telefonica.com";
 		}else{
-			correo = "responsable.obadirecto@telefonica.com";
+			correo = "responsable.obaindirecto@telefonica.com";
 		}
 	}else if (document.getElementById("AMLT").checked) {
+		circuito = "AMLT";
 		if(aviso == 1){
-			correo = "escalado.obadirecto@telefonica.com";
+			correo = "escalado.amlt@telefonica.com";
 		}else{
-			correo = "responsable.obadirecto@telefonica.com";
+			correo = "responsable.amlt@telefonica.com";
 		}
 	}else if (document.getElementById("NEBA").checked) {
+		circuito = "NEBA";
 		if(aviso == 1){
-			correo = "escalado.obadirecto@telefonica.com";
+			correo = "escalado.neba@telefonica.com";
 		}else{
-			correo = "responsable.obadirecto@telefonica.com";
+			correo = "responsable.neba@telefonica.com";
 		}
 	}
 
-	plantilla.innerHTML = "<br /> asunto:" + p1 + " Oceane: " + oceane.value + " - " + empresa.value +  "<br />"
-	 + "<br />" + " - nº de ticket de ORANGE: " + oceane.value
-	 + "<br />" + " - datos de cliente: <br />"
-	 + "    nombre: " + nombre.value  + "<br />"
-	 + "		dirección: " + direccion.value + "<br />"
-	 + "    Telefono: " + telefono.value  + "<br />"
-	 + " - horario de atención del cliente: " + horario.value + "<br />"
-	 + " - plantilla: no <br />  - modelo de equipo instalado (incluida tarjetería): " + modelo.value
-	 + "<br />" + " - tipo de aviso: " + aviso
-	 + "<br />" + " - descripción de la solicitud: <br />"
-	 + descripcion.value
-	 + "<br /";
+	plantilla.innerHTML = "<strong>Correo a enviar: </strong>" + correo 
+	 + "<br /> <br /><strong>asunto:</strong> ORANGE # " + nivel + " # " + admin.value + " // " + circuito + " // " + incidencia.value + " // " + oceane.value
+	 + "<br /> <br />Tenemos abierta una incidencia por " + causa.value + " con el número administrativo " + admin.value 
+	 + " ,  el número de secuencia en Telefónica " + incidencia.value + " y el número de incidencia de Orange " + oceane.value + "."
+ 	 + "<br /><br/ > La incidencia está abierta desde el pasado día " + fecha.value + " a las " + horario.value + ", y todavía no hemos recibido información alguna por parte de los técnicos de Telefónica."
+     + "<br /><br /> Necesitamos que se revise lo antes posible, ya que llevamos mas de 24 horas con la incidencia abierta."
+     + "<br /><br /> Solicitamos escalado a " + nivel + ".";
+
 }
