c3f8e049e5de26350d376517764fd5030bb0ef87
Alberto <tranksirc@gmail.com>
2019-01-11 14:33:25 +0100

    se arregla el problema para generar la plantilla en personalizados.js

--- personalizados.js
+++ personalizados.js
@@ -15,47 +15,49 @@ creaccion.addEventListener("click", generarPlantilla);
 
 function generarPlantilla() {
 
-var indice = document.formul.cliente.selectedIndex;
-var cliente = document.formul.cliente.options[indice].text;
-var correo = "";
+	var indice = document.formulario2.cliente.selectedIndex;
+	var cliente = document.formulario2.cliente.options[indice].text;
+	var correo = "";
 	//seleccionar tipo de cliente.
-switch (indice) {
-	case 1:
-		correo = "Nuria.Fontanillas@wurth.es; Esteve.Boladeras@wurth.es; josep.perez@wurth.es";
-		break;
-	case 2:
-		break;
-	case 3:
-		break;
-	case 4:
-		break;
-	case 5:
-		break;
-	case 6:
-		break;
-	case 7:
-		break;
-	case 8:
-		break;
-	case 9:
-		break;
-	case 10:
-		break;
-	case 11:
-		break;
-	case 12:
-		break;
-	case 13:
-		break;
-	default:
-}
+	switch (indice) {
+		case 0:
+			correo = "Nuria.Fontanillas@wurth.es; Esteve.Boladeras@wurth.es; josep.perez@wurth.es";
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+		case 10:
+			break;
+		case 11:
+			break;
+		case 12:
+			break;
+		default:
+	}
 
-	plantilla.innerHTML = "<strong>Correo a enviar: </strong>" + correo + " " + "CC: scesoporten2.es@orange.com; "
-	+"<br /><br /><strong>Asunto: <strong>" + clente + " " + sede.value + " ID: " + oceane.value
+	plantilla.innerHTML = "<strong>Correo a enviar: </strong>" + correo + " "
+	+"<br /><strong>CC: </strong>scesoporten2.es@orange.com; "
+	+"<br /><br /><strong>Asunto: </strong>" + cliente + " " + sede.value + " ID: " + oceane.value
 	+"<br /><br /><br /> Tal y como hemos hablado se ha procedido a la apertura de incidencia para su revisión:"
-	+"<br /><br /> Incidencia: " + oceane.value +
+	+"<br /><br /> Incidencia: " + oceane.value
 	+"<br />	Sede: " + sede.value
 	+"<br /> Servicio: " + servicio.value
 	+"<br /> Mnemónico CPE: " + nemonico.value
 	+"<br /> Síntoma: " + sintoma.value
 	+"<br /><br /> un saludo";
+}
