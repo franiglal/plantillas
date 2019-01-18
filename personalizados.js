ce71a84c55e13d2f93e876e4258e8a53139ff892
Alberto <tranksirc@gmail.com>
2019-01-18 10:54:26 +0100

    pagina web personalizados creado. a espera de informacion de dos clientes

--- personalizados.js
+++ personalizados.js
@@ -24,28 +24,40 @@ function generarPlantilla() {
 			correo = "Nuria.Fontanillas@wurth.es; Esteve.Boladeras@wurth.es; josep.perez@wurth.es";
 			break;
 		case 1:
+			correo = "pnte de Eva";
 			break;
 		case 2:
+			correo = "luis.seilices@otis.com";
 			break;
 		case 3:
+			correo = "vortiz@centrofarmaceutico.es; alopez@centrofarmaceutico.es; fguijarro@centrofarmaceutico.es; jortiz@centrofarmaceutico.es; jmaza@centrofarmaceutico.es";
 			break;
 		case 4:
+			correo = "pnte de Eva";
 			break;
 		case 5:
+			correo = "espanainformatica@kiabi.com";
 			break;
 		case 6:
+			correo = "lista.atnetworkservicesvoice@axa-tech.com; jesus.zaballos@axa.es; olga.yuste@axa.es";
 			break;
 		case 7:
+			correo = "fjvalles@sonaemc.com; suporte.somaemc@nos.pt; nuno.sanches@parceiros.nos.pt";
 			break;
 		case 8:
+			correo = "fernandojavier.jimenez@engie.com; joseantonio.perez@engie.com; antonio.mendiola@engie.com; esther.pina@engie.com";
 			break;
 		case 9:
+			correo = "fernandojavier.jimenez@engie.com; joseantonio.perez@engie.com; antonio.mendiola@engie.com; esther.pina@engie.com";
 			break;
 		case 10:
+			correo = "fernandojavier.jimenez@engie.com; joseantonio.perez@engie.com; antonio.mendiola@engie.com; esther.pina@engie.com";
 			break;
 		case 11:
+			correo = "fernandojavier.jimenez@engie.com; joseantonio.perez@engie.com; antonio.mendiola@engie.com; esther.pina@engie.com";
 			break;
 		case 12:
+			correo = "fernandojavier.jimenez@engie.com; joseantonio.perez@engie.com; antonio.mendiola@engie.com; esther.pina@engie.com";
 			break;
 		default:
 	}
